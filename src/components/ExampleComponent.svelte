590b14cdc562609e9efd09ec76254bc38a72f69e
Adam Manuel <68038126+AdamManuel-dev@users.noreply.github.com>
2020-11-30 15:28:43 -0600

    Initial commit

---
+++ src/components/ExampleComponent.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let title: string;
+	export let paragraph: string;
+</script>
+
+<svelte:options immutable />
+
+<h1 class="text-3xl text-blue-500 font-extralight">{title}</h1>
+<p class="mt-4 text-gray-700">{paragraph}</p>
